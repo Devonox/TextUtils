f95b3f520da344568d7dfea4459eef6a56908d2c
ArghadeepMandal <56314700+Devonox@users.noreply.github.com>
2022-02-07 16:00:09 +0530

    commit on 'master'

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,135 @@
 import React from "react";
 
 export default function About(props) {
-
   return (
     <>
       <div className="container">
-        <h1 className="my-3" style={{color: props.mode === "dark" ? "#ed9511" : "black"}}>About Me</h1>
+        <h1
+          className="my-3"
+          style={{ color: props.mode === "dark" ? "#ed9511" : "black" }}
+        >
+          About Me
+        </h1>
         <div className="accordion" id="accordionExample">
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingOne">
               <button
-                className="accordion-button"
+                className="accordion-button collapsed"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#collapseOne"
                 aria-expanded="true"
                 aria-controls="collapseOne"
-                style={{color: "black", fontSize: 20,backgroundColor: props.mode === "dark" ? "rgb(245 148 148)" : "#e7f1ff"}}
+                style={{
+                  color: "black",
+                  fontSize: 20,
+                  backgroundColor:
+                    props.mode === "dark" ? "rgb(245 148 148)" : "#e7f1ff",
+                }}
               >
-                <b> Hi! My name is Arghadeep Mandal</b>
+                <b>Analyze Your text</b>
               </button>
             </h2>
             <div
               id="collapseOne"
-              className="accordion-collapse collapse show"
+              className="accordion-collapse collapse"
               aria-labelledby="headingOne"
               data-bs-parent="#accordionExample"
             >
-              <div className="accordion-body" style={{color: props.mode === "dark" ? "#ed9511" : "black", backgroundColor:props.mode==="dark"?"rgb(1 7 26)":"white"}}>
-                <strong>I am a Web Devoloper.</strong> I have also done some
-                other stuffs. Like i have made a virtual assistant with python.
-                 I have also made many projects with Python, among those my
-                favourite on is the virtual Keyboard made with python with
-                OpenCV Library. Apart from that ,I have a very keen interest in
-                Android Devolopment and also have a very good knowledge of it.
+              <div
+                className="accordion-body"
+                style={{
+                  color: props.mode === "dark" ? "#ed9511" : "black",
+                  backgroundColor:
+                    props.mode === "dark" ? "rgb(1 7 26)" : "white",
+                }}
+              >
+                Textutils gives you a way to analyze your text quickly and
+                efficiently. Be it word count, character count or
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+                style={{
+                  color: "black",
+                  fontSize: 20,
+                  backgroundColor:
+                    props.mode === "dark" ? "rgb(245 148 148)" : "#e7f1ff",
+                }}
+              >
+                <b>Free to use </b>
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div
+                className="accordion-body"
+                style={{
+                  color: props.mode === "dark" ? "#ed9511" : "black",
+                  backgroundColor:
+                    props.mode === "dark" ? "rgb(1 7 26)" : "white",
+                }}
+              >
+                TextUtils is a free character counter tool that provides instant
+                character count & word count statistics for a given text.
+                TextUtils reports the number of words and characters. Thus it is
+                suitable for writing text with word/ character limit.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+                style={{
+                  color: "black",
+                  fontSize: 20,
+                  backgroundColor:
+                    props.mode === "dark" ? "rgb(245 148 148)" : "#e7f1ff",
+                }}
+              >
+                <b>Browser Compatible </b>
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div
+                className="accordion-body"
+                style={{
+                  color: props.mode === "dark" ? "#ed9511" : "black",
+                  backgroundColor:
+                    props.mode === "dark" ? "rgb(1 7 26)" : "white",
+                }}
+              >
+                This word counter software works in any web browsers such as
+                Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to
+                count characters in facebook, blog, books, excel document, pdf
+                document, essays, etc.
               </div>
             </div>
           </div>
